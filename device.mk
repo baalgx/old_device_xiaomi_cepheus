3ed6380147bbf9dc5a49015fe17bb6af9caf2bf4
balgxmr <jose@pixelos.net>
2024-11-16 19:02:18 -0500

    Revert "cepheus: Switch to xiaomi vibrator aidl"
    This reverts commit 7bf6cdb6e28173477214f02faa0f5e0bbc6b5cb4.

--- device.mk
+++ device.mk
@@ -563,10 +563,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi
-
-PRODUCT_COPY_FILES += \
-    hardware/xiaomi/aidl/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    vendor.qti.hardware.vibrator.service
 
 # VNDK
 PRODUCT_PACKAGES += \
